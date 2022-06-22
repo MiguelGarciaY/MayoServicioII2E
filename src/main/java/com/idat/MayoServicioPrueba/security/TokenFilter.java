a717a37876460cc090ee9665c4a3827431bede71
Miguelgarci4 <migue_garci4@hotmail.com>
2022-06-21 22:16:17 -0500

    Subiendo primeros cambios de la configuracion con JWT Security

---
+++ src/main/java/com/idat/MayoServicioPrueba/security/TokenFilter.java
@@ -0,0 +1,28 @@
+package com.idat.MayoServicioPrueba.security;
+
+import java.io.IOException;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+public class TokenFilter extends OncePerRequestFilter{
+
+	@Autowired
+	private UserDetailService service;
+	
+	@Autowired
+	private JWTUtil util;	
+	
+	@Override
+	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		
+	}
+	
+}
